fa770c2b11eab8ec4fa71b016f990cee17979520
magoma39 <mg4683601@gmail.com>
2025-09-12 08:33:41 +0300

    fixing

--- Activity: findian.go/findian.go
+++ Activity: findian.go/findian.go
@@ -1,22 +1,27 @@
 package main
+
 import (
-	"fmt"
-	"bufio"
-	"os"
-	"strings"
+    "bufio"
+    "fmt"
+    "os"
+    "strings"
 )
 
-
 func main() {
-	fmt.Println("Enter a string:")
-	reader := bufio.NewReader(os.Stdin)
-	input, _ := reader.ReadString('\n')
-	input = strings.ToLower(strings.TrimSpace(input))
-	if strings.HasPrefix(input, "i") &&
-    strings.HasSuffix(input, "n") &&
-    strings.Contains(input, "a") {
-		fmt.Println("Found!")
-		} else {
-			fmt.Println("Not Found!")
-		}		
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Print("Enter a string: ")
+    input, _ := reader.ReadString('\n')
+
+    input = strings.TrimSpace(input)
+    input = strings.ToLower(input)
+
+    startsWithI := strings.HasPrefix(input, "i")
+    endsWithN := strings.HasSuffix(input, "n")
+    containsA := strings.Contains(input, "a")
+
+    if startsWithI && endsWithN && containsA {
+        fmt.Println("Found!")
+    } else {
+        fmt.Println("Not Found!")
+    }
 }
